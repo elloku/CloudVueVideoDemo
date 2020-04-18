13d01cdbd93211b2ac409295521e9e2826063983
songxuanlong <songxuanlong@mediinfo.com.cn>
2020-04-18 10:16:48 +0800

    新增gatewaym模块

---
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -0,0 +1,28 @@
+package com.example.gateway;
+
+//import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+@ComponentScan("com.example")
+//@MapperScan("com.example.server.mapper")
+public class GatewayApplication {
+
+	private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(GatewayApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+
+}
+
