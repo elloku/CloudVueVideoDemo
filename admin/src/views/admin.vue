3620454841fe8756c879ad86831774279c3d25fe
songxuanlong <songxuanlong@mediinfo.com.cn>
2020-05-15 23:53:07 +0800

    add welcome.vue add /admin/welcome route with router-view

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -2,7 +2,8 @@
     <div>
         <div id="navbar" class="navbar navbar-default          ace-save-state">
             <div class="navbar-container ace-save-state" id="navbar-container">
-                <button type="button" class="navbar-toggle menu-toggler pull-left" id="menu-toggler" data-target="#sidebar">
+                <button type="button" class="navbar-toggle menu-toggler pull-left" id="menu-toggler"
+                        data-target="#sidebar">
                     <span class="sr-only">Toggle sidebar</span>
 
                     <span class="icon-bar"></span>
@@ -58,7 +59,8 @@
                                                 </div>
 
                                                 <div class="progress progress-mini">
-                                                    <div style="width:35%" class="progress-bar progress-bar-danger"></div>
+                                                    <div style="width:35%"
+                                                         class="progress-bar progress-bar-danger"></div>
                                                 </div>
                                             </a>
                                         </li>
@@ -71,7 +73,8 @@
                                                 </div>
 
                                                 <div class="progress progress-mini">
-                                                    <div style="width:15%" class="progress-bar progress-bar-warning"></div>
+                                                    <div style="width:15%"
+                                                         class="progress-bar progress-bar-warning"></div>
                                                 </div>
                                             </a>
                                         </li>
@@ -84,7 +87,8 @@
                                                 </div>
 
                                                 <div class="progress progress-mini progress-striped active">
-                                                    <div style="width:90%" class="progress-bar progress-bar-success"></div>
+                                                    <div style="width:90%"
+                                                         class="progress-bar progress-bar-success"></div>
                                                 </div>
                                             </a>
                                         </li>
@@ -184,7 +188,8 @@
                                     <ul class="dropdown-menu dropdown-navbar">
                                         <li>
                                             <a href="#" class="clearfix">
-                                                <img src="assets/images/avatars/avatar.png" class="msg-photo" alt="Alex's Avatar" />
+                                                <img src="assets/images/avatars/avatar.png" class="msg-photo"
+                                                     alt="Alex's Avatar"/>
                                                 <span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Alex:</span>
@@ -201,7 +206,8 @@
 
                                         <li>
                                             <a href="#" class="clearfix">
-                                                <img src="assets/images/avatars/avatar3.png" class="msg-photo" alt="Susan's Avatar" />
+                                                <img src="assets/images/avatars/avatar3.png" class="msg-photo"
+                                                     alt="Susan's Avatar"/>
                                                 <span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Susan:</span>
@@ -218,7 +224,8 @@
 
                                         <li>
                                             <a href="#" class="clearfix">
-                                                <img src="assets/images/avatars/avatar4.png" class="msg-photo" alt="Bob's Avatar" />
+                                                <img src="assets/images/avatars/avatar4.png" class="msg-photo"
+                                                     alt="Bob's Avatar"/>
                                                 <span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Bob:</span>
@@ -235,7 +242,8 @@
 
                                         <li>
                                             <a href="#" class="clearfix">
-                                                <img src="assets/images/avatars/avatar2.png" class="msg-photo" alt="Kate's Avatar" />
+                                                <img src="assets/images/avatars/avatar2.png" class="msg-photo"
+                                                     alt="Kate's Avatar"/>
                                                 <span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Kate:</span>
@@ -252,7 +260,8 @@
 
                                         <li>
                                             <a href="#" class="clearfix">
-                                                <img src="assets/images/avatars/avatar5.png" class="msg-photo" alt="Fred's Avatar" />
+                                                <img src="assets/images/avatars/avatar5.png" class="msg-photo"
+                                                     alt="Fred's Avatar"/>
                                                 <span class="msg-body">
 													<span class="msg-title">
 														<span class="blue">Fred:</span>
@@ -280,7 +289,7 @@
 
                         <li class="light-blue dropdown-modal">
                             <a data-toggle="dropdown" href="#" class="dropdown-toggle">
-                                <img class="nav-user-photo" src="assets/images/avatars/user.jpg" alt="Jason's Photo" />
+                                <img class="nav-user-photo" src="assets/images/avatars/user.jpg" alt="Jason's Photo"/>
                                 <span class="user-info">
 									<small>Welcome,</small>
 									Jason
@@ -834,7 +843,8 @@
                 </ul><!-- /.nav-list -->
 
                 <div class="sidebar-toggle sidebar-collapse" id="sidebar-collapse">
-                    <i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state" data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
+                    <i id="sidebar-toggle-icon" class="ace-icon fa fa-angle-double-left ace-save-state"
+                       data-icon1="ace-icon fa fa-angle-double-left" data-icon2="ace-icon fa fa-angle-double-right"></i>
                 </div>
             </div>
 
@@ -856,7 +866,8 @@
                         <div class="nav-search" id="nav-search">
                             <form class="form-search">
 								<span class="input-icon">
-									<input type="text" placeholder="Search ..." class="nav-search-input" id="nav-search-input" autocomplete="off" />
+									<input type="text" placeholder="Search ..." class="nav-search-input"
+                                           id="nav-search-input" autocomplete="off"/>
 									<i class="ace-icon fa fa-search nav-search-icon"></i>
 								</span>
                             </form>
@@ -884,27 +895,32 @@
                                     </div>
 
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-navbar" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state"
+                                               id="ace-settings-navbar" autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-navbar"> Fixed Navbar</label>
                                     </div>
 
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-sidebar" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state"
+                                               id="ace-settings-sidebar" autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-sidebar"> Fixed Sidebar</label>
                                     </div>
 
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-breadcrumbs" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state"
+                                               id="ace-settings-breadcrumbs" autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-breadcrumbs"> Fixed Breadcrumbs</label>
                                     </div>
 
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-rtl"
+                                               autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-rtl"> Right To Left (rtl)</label>
                                     </div>
 
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state" id="ace-settings-add-container" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2 ace-save-state"
+                                               id="ace-settings-add-container" autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-add-container">
                                             Inside
                                             <b>.container</b>
@@ -914,17 +930,20 @@
 
                                 <div class="pull-left width-50">
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-hover"
+                                               autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-hover"> Submenu on Hover</label>
                                     </div>
 
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-compact"
+                                               autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-compact"> Compact Sidebar</label>
                                     </div>
 
                                     <div class="ace-settings-item">
-                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-highlight" autocomplete="off" />
+                                        <input type="checkbox" class="ace ace-checkbox-2" id="ace-settings-highlight"
+                                               autocomplete="off"/>
                                         <label class="lbl" for="ace-settings-highlight"> Alt. Active Item</label>
                                     </div>
                                 </div><!-- /.pull-left -->
@@ -934,7 +953,7 @@
                         <div class="row">
                             <div class="col-xs-12">
                                 <!-- PAGE CONTENT BEGINS -->
-
+                                <router-view/>
                                 <!-- PAGE CONTENT ENDS -->
                             </div><!-- /.col -->
                         </div><!-- /.row -->
@@ -980,13 +999,13 @@
 
     export default {
         name: 'login',
-        mounted:function(){
+        mounted: function () {
             $('body').removeClass('login-layout');
             $('body').attr('class', 'no-skin');
 
         },
-        methods:{
-            login(){
+        methods: {
+            login() {
                 this.$router.push("/admin")
             }
         }
---
+++ admin/src/views/admin/welcome.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>我言秋日胜春朝</h1>
+    </div>
+</template>
