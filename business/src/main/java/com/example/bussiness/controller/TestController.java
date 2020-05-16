5fd9e946e88205e73ca77c344b7ab9050a71e048
songxuanlong <songxuanlong@mediinfo.com.cn>
2020-05-16 10:28:49 +0800

    add business module

---
+++ business/src/main/java/com/example/bussiness/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.example.bussiness.controller;
+
+import com.example.server.domain.Test;
+import com.example.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @author ：vivaciousis
+ * @date ：Created in 2020/4/18 0:27
+ * @description：
+ */
+
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")
+    public List<Test> test(){
+        return testService.GetList();
+    }
+}
