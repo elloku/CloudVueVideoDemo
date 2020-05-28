1fd8befb11b80ca24fa304d25cdefb5949d56e44
vivacious <songxuanlong@mediinfo.com.cn>
2020-05-28 22:55:29 +0800

    use gateway implement route forwarding

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -88,7 +88,7 @@
         methods:{
             Getlist() {
                 let _this = this;
-                _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/getlist').then((response)=>{
+                _this.$ajax.get('http://127.0.0.1:9000/business/admin/chapter/getlist').then((response)=>{
                     console.log("查询大章列表结果：", response);
                     _this.chapters = response.data;
                 })
