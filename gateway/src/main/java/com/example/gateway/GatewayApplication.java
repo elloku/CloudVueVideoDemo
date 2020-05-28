9d54d4bbe7555efeafc80616aaeae240321908be
vivacious <songxuanlong@mediinfo.com.cn>
2020-05-28 23:10:42 +0800

    1. 在gateway中增加解决跨域问题的方案，并注释掉CrosConfig 2. 使用名称进行路由转发lb://business

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,13 +1,19 @@
 package com.example.gateway;
 
 //import org.mybatis.spring.annotation.MapperScan;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,14 +21,36 @@ import org.springframework.core.env.Environment;
 //@MapperScan("com.example.server.mapper")
 public class GatewayApplication {
 
-	private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(GatewayApplication.class);
-		Environment env = app.run(args).getEnvironment();
-		LOG.info("启动成功！！");
-		LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("Gateway地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+
+    /**
+     * 配置跨域
+     *
+     * @return
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
 
 }
 
--- server/src/main/java/com/example/server/config/CorsConfig.java
+++ server/src/main/java/com/example/server/config/CorsConfig.java
@@ -9,8 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author ：vivaciousis
  * @date ：Created in 2020/5/28 22:37
  * @description：解决跨域问题
+ * 这部分注释掉是因为采用了GateWay中的解决方案。
+ * 就不需要使用这个配置来解决跨域问题了。
+ * 而且如果和GateWay中的一起使用好像还会冲突
  */
-@Configuration
+/*@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
@@ -22,4 +25,4 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowCredentials(true)
                 .maxAge(3600); // 1小时内不需要再预检（发OPTIONS请求）
     }
-}
+}*/
