ca4ba57b62a435dbf2835a7d7c62547169ae5cf6
vivacious <songxuanlong@mediinfo.com.cn>
2020-05-31 20:50:19 +0800

    1. 增加工具类tool.js和校验类validator.js 2. 大章保存非空和长度增加校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -124,6 +124,12 @@
             },
             save(page) {
                 let _this = this;
+                // 保存校验
+                if (!Validator.require(_this.chapter.name, "名称")
+                    || !Validator.require(_this.chapter.courseId, "课程ID")
+                    || !Validator.length(_this.chapter.courseId, "课程ID", 1, 8)) {
+                    return;
+                }
                 Loading.show();
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/save', _this.chapter).then((response) => {
                     console.log("保存大章列表结果：", response);
@@ -133,7 +139,6 @@
                         $("#form-modal").modal("hide");
                         _this.list(1);
                         Toast.success("保存成功！");
-                        T
                     }
                 })
             },
